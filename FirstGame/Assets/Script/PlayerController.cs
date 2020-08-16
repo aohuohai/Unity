f1a8724a6c916b3e2013180fa347abed43dfbef4
林锦鸿 <2468395740ljh@gmail.com>
2020-08-17 00:36:19 +0800

    unity

---
+++ FirstGame/Assets/Script/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    public Collider2D coll;
+    public float speed;
+    public float jumpforce;
+    public LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        SwitchAima();
+    }
+    //void FixedUpdate()  
+    //{
+    //    Movement();
+    //}
+
+    void Movement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float faceDirection = Input.GetAxisRaw("Horizontal");
+        //控制水平移动
+        if (horizontalMove != 0)
+        {
+            rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(faceDirection));
+        }
+        //控制面向方向
+        if (faceDirection != 0)
+        {
+            transform.localScale = new Vector3(faceDirection, 1, 1);
+        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+        //}
+        //控制跳跃
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+            anim.SetBool("jumping", true);
+            anim.SetBool("falling", false);
+        }
+    }
+
+    void SwitchAima()
+    {
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("falling", true);
+            }
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling", false);
+        }
+    }
+}
